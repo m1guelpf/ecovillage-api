71c1aa756cce7faeb1d8d9c8703d226d096f9d46
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-08-28 06:18:40 +0200

    Add search by title endpoint

--- app/Http/Controllers/VillageController.php
+++ app/Http/Controllers/VillageController.php
@@ -7,47 +7,28 @@ use Illuminate\Http\Request;
 
 class VillageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Village::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         abort(501);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Village $village)
     {
         return $village;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Village $village)
     {
         abort(501);
+    }
+
+    public function searchByTitle(Request $request)
+    {
+        return Village::where('name', 'like', "%{$request->input('title')}%")->get();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('villages/search', 'VillageController@searchByTitle');
 Route::resource('villages', 'VillageController')->only('index', 'show');
